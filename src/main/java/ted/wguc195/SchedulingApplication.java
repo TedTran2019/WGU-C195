b3df0ba5ba16ff2cb99adb5de8fbcea7bf41589d
Ted Tran <tedtran2019@gmail.com>
2022-12-17 04:04:51 -0800

    Planning is relatively done, but now I kind of want to see Software 1 graded so I can revise it before beginning on Software 2

--- src/main/java/ted/wguc195/SchedulingApplication.java
+++ src/main/java/ted/wguc195/SchedulingApplication.java
@@ -1,5 +1,4 @@
 package ted.wguc195;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -13,18 +12,49 @@ public class SchedulingApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SchedulingApplication.class.getResource("/views/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Scheduling Application");
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     * Login Form: username, password, displays lang and zoneID.
+     * Let's do a drop down or combo box for the language for easy testing + practice
+     * Main form: Customers and Appointments (Can be added, updated, deleted) (Table views)
+     * - All customer's appointments must be deleted b4 customer can be deleted
+     * - Text fields collect: name, address, postal code, phone #
+     * - IDs are auto-generated, states/provinces and country data are collected using combo boxes
+     * - Country and state/provinces are prepopulated combo boxes, with first list filtering the second
+     *
+     * - Drop down menu OR combo box for contact name for appointment
+     * - ID autogen, title, description, location, contact, type, start-date and time, end-date and time
+     *
+     * - User can view appointment schedules by month/week using tableview (tabs or radio buttons to pick month/week to filter by)
+     * - Some way to enter date? (Calendar? Text field?)
+     *
+     * - Allow users to adjust appointment times.
+     * - Times are stored in UTC, displayed in local time, and checked against EST business hours
+     *
+     *  - Implement input validation/logical error checks as specified in part D of the rubric
+     *  - Implement alert when there's an appointment within 15 mins of a user's login (15 mins within local time)
+     *
+     *  Provide reports on the following:
+     *  - total # of customer appointments by type and month
+     *  - schedule for each contact in organization (sort by earliest to latest)
+     *  - Additional report based on user login date and time stamp from login attempts
+     *
+     *  - Two lambdas in code
+     *  - Track user activity by recording all login attempts (along w/ date and timestamps + success) to a text file
+     *  - Javadocs
+     *  - README.txt (not md?) with a bunch of information
+     * */
     public static void main(String[] args) {
         Locale french = new Locale("fr", "FR");
         Locale.setDefault(french);
         try {
             ResourceBundle rb = ResourceBundle.getBundle("bundles/lang", Locale.getDefault());
-            if (Locale.getDefault().getLanguage().equals("en")) {
+            if (Locale.getDefault().getLanguage().equals("fr")) {
                 System.out.println(rb.getString("hello"));
             }
         } catch (Exception e) {
