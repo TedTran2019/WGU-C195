20541fc75d13b9e51bbab55160d21b6d785d33a4
Ted Tran <tedtran2019@gmail.com>
2023-02-07 14:22:38 -0800

    Completed base UI for reports and forming skeleton for the controllers, going to fill it out now! This shouldn't be too bad

--- src/main/java/ted/wguc195/controllers/ReportsController.java
+++ src/main/java/ted/wguc195/controllers/ReportsController.java
@@ -1,4 +1,49 @@
 package ted.wguc195.controllers;
 
-public class ReportsController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleGroup;
+
+import java.io.IOException;
+
+public class ReportsController extends BaseController {
+    @FXML
+    protected ToggleGroup toggleReports;
+
+    @FXML
+    protected Label labelReportsCounter;
+
+    @FXML
+    private ComboBox<?> comboBoxReports;
+
+    @FXML
+    private TableView<?> tableViewReports;
+
+    @FXML
+    protected void onActionContactSchedule(ActionEvent event) throws IOException {
+        switchScene(event, "/views/reports/ReportsContact.fxml");
+    }
+
+    @FXML
+    protected void onActionCustAppMonth(ActionEvent event) throws IOException {
+        switchScene(event, "/views/reports/ReportsMonth.fxml");
+    }
+
+    @FXML
+    protected void onActionCustAppType(ActionEvent event) throws IOException {
+        switchScene(event, "/views/reports/ReportsType.fxml");
+    }
+
+    @FXML
+    protected void onActionUserLogins(ActionEvent event) throws IOException {
+        switchScene(event, "/views/reports/ReportsUser.fxml");
+    }
+
+    public void initialize() {
+        comboBoxReports.setVisible(false);
+        labelReportsCounter.setVisible(false);
+    }
 }
---
+++ src/main/java/ted/wguc195/controllers/reports/ReportsContactController.java
@@ -0,0 +1,23 @@
+package ted.wguc195.controllers.reports;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import ted.wguc195.controllers.ReportsController;
+
+public class ReportsContactController extends ReportsController {
+    @FXML
+    private ComboBox<?> comboBoxReports;
+
+    @FXML
+    private TableView<?> tableViewReports;
+
+    @FXML
+    void onActionComboBox(ActionEvent event) {
+
+    }
+
+    public void initialize() {
+    }
+}
